5aeca505bcff63e0ed325c003bd780c2bf621b05
Alnfonseca <afonsecadesantana.com>
2024-02-28 15:52:40 -0300

    formulário de cadastro

--- config.php
+++ config.php
@@ -7,11 +7,11 @@ $dbName = 'siteclientes';
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-if($conexao -> connect_errno){
+/*if($conexao -> connect_errno){
     echo "Erro";
 }
 else{
     echo "Conexão efetuada com sucesso";
 }
 
-?>
+?>*/
--- formulario.php
+++ formulario.php
@@ -1,10 +1,43 @@
 <?php 
 if(isset($_POST['submit'])){
-    print_r($_POST['nome']);
+
+    /*  TESTANDO O RESTORNO DOS DADOS
+    print_r('Nome: '. $_POST['nome']);
     print_r('<br>');
-    print_r($_POST['email']);
+    print_r('E-amil: ' . $_POST['email']);
     print_r('<br>');
-    print_r($_POST['telefone']);
+    print_r('Senha: ' . $_POST['senha']);
+    print_r('<br>');
+    print_r('Telefone: ' . $_POST['telefone']);
+    print_r('<br>');
+    print_r('Gênero: ' . $_POST['genero']);
+    print_r('<br>');
+    print_r('Data de Nascimento: ' . $_POST['data_nascimento']);
+    print_r('<br>');
+    print_r('Endereço: ' . $_POST['endereco']);
+    print_r('<br>');
+    print_r('Cidade: ' . $_POST['cidade']);
+    print_r('<br>');
+    print_r('Estado: ' . $_POST['estado']);
+    print_r('<br>');
+    */
+
+    include_once('config.php');
+
+    $nomecompleto = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $telefone = $_POST['telefone'];
+    $genero = $_POST['genero'];
+    $data_nascimento = $_POST['data_nascimento'];
+    $endereco = $_POST['endereco'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+
+    $result = mysqli_query($conexao,
+    "INSERT INTO formulariocadastro(nomecompleto, email, senha, telefone, genero, data_nascimento, endereco, cidade, estado)
+    values('$nomecompleto', '$email', '$senha', $telefone, '$genero', '$data_nascimento', '$endereco', '$cidade', '$estado')"
+    );
 }
 
 ?>
